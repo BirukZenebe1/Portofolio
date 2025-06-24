55c2f8cf27e8913915ce143eb9459b77c9b40b4f
biruk zenebe <brk@biruks-MacBook-Air.local>
2025-06-24 18:03:44 +0200

    Link updated 2

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -33,7 +33,7 @@ export default function Portfolio() {
       title: "Persona AI Recommender",
       description:
         "E-commerce recommender built with JavaScript, HTML/CSS, and Python to suggest items based on user history.",
-      link: "https://github.com/BirukZenebe1/41697_..._persona_ai_project",
+      link: "https://personabkmlys.netlify.app",
       image:
         "https://images.unsplash.com/photo-1589254065909-b7086229d08c?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
